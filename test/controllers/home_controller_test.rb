01b9408b47f7966f8571320f86a7bb848f0628dd
nexspro <nexspro@ik.me>
2026-06-24 21:25:27 +0000

    Test home page content (#14)

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,5 +4,8 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
+    assert_select "h1", "NexsNews"
+    assert_select "p", "A technical news and publishing platform built with Ruby on Rails."
+    assert_select "p", "The application is running successfully."
   end
 end
