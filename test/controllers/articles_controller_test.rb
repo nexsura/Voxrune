158c679dcdd4e28eb968a774e6724fd22e616ae4
nexspro <nexspro@ik.me>
2026-06-29 23:44:52 +0200

    Improve article published date markup

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -7,6 +7,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "h1", "Articles"
     assert_includes response.body, articles(:two).title
+    assert_select "time[datetime=?]", articles(:two).published_at.iso8601
     assert_no_match articles(:one).title, response.body
   end
 end
