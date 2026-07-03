e243849b2e8a0cd2d39742d59476b4f673de78cb
nexspro <nexspro@ik.me>
2026-07-03 22:51:13 +0200

    Feature/articl detail (#22)
    * Improve article published date markup
    
    * Add published article detail page
    
    * Resolve article detail merge conflict

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -7,7 +7,18 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "h1", "Articles"
     assert_includes response.body, articles(:two).title
+    assert_select "a[href=?]", article_path(articles(:two)), articles(:two).title
     assert_select "time[datetime=?]", articles(:two).published_at.iso8601
     assert_no_match articles(:one).title, response.body
+  end
+
+  test "should show published article" do
+    get article_url(articles(:two))
+
+    assert_response :success
+    assert_select "h1", articles(:two).title
+    assert_select "a[href=?]", articles_path, "Back to articles"
+    assert_select "time[datetime=?]", articles(:two).published_at.iso8601
+    assert_includes response.body, articles(:two).body
   end
 end
